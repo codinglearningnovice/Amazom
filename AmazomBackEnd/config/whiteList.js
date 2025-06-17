f8320e408b0c34a3116b28713d2b427563f3ca52
codinglearningnovice <palnicey2021@outlook.com>
2025-06-17 15:44:51 +0100

    updated

--- AmazomBackEnd/config/whiteList.js
+++ AmazomBackEnd/config/whiteList.js
@@ -1,9 +1,8 @@
 const whiteList = [
-  "https://www.amazom.com",
-  "https://127.0.0.1:5500",
+  "https://amazomecommerc-general-frontend.onrender.com",
   "http://localhost:3600",
   "http://localhost:5174",
-  "http://localhost:5173", "https://amazomecommerc-general-frontend.onrender.com/",
+  "http://localhost:5173",
 ];
 
 module.exports = whiteList;
