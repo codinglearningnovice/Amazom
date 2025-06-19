b5527e01f3ebf4dee1a208a5fa1de164420dfa65
codinglearningnovice <palnicey2021@outlook.com>
2025-06-19 17:08:09 +0100

    updated

--- Amazom/src/axios.jsx
+++ Amazom/src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://amazomecommerc.onrender.com",
+  baseURL: "http://localhost:3600",
 });
 
 export default instance;
